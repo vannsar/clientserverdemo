b1ea5afd5972dc28a2302b710340897183cfc32f
Vannarith Sar <vannsar@gmail.com>
2019-08-06 12:58:41 -0700

    Added fav foods

--- client/main.js
+++ client/main.js
@@ -2,18 +2,33 @@ $(document).ready( startApp );
 
 function startApp(){
   getData();
+  getFoodData();
 }
 
-function getData(){
+function getData() {
   var settings = {
     url: '../server/getnames.json',
     method: 'get',
     dataType: 'json',
     success: handleDataFromServer
   }
-  $.ajax( settings );
+  $.ajax(settings);
+}
+
+function getFoodData() {
+  var settings = {
+    url: '../server/getfood.json',
+    method: 'get',
+    dataType: 'json',
+    success: handleDataFromServer
+  }
+  $.ajax(settings);
 }
 
 function handleDataFromServer( response ){
+  console.log(response);
+}
+
+function handleFoodFromServer ( response ){
   console.log(response);
 }
